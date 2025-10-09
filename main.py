c10b7dd3ca5338d54a638d06f8013356d2966676
Ofek-Hodis <ofkhod@gmail.com>
2025-10-09 20:42:11 +0200

    Base app structure

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
-import requests  # Imported to have contact with
+import sys
+from weather import get_weather
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QPushButton, QVBoxLayout, QLineEdit
 from datetime import datetime, timezone, timedelta
-from dotenv import load_dotenv  # Importing the function allowing to import data from a .env file
-import os  # Allows Python to interact with the os, needed for the communication with the .env file
-# Importing a command used to navigate to a specific file type (will be used to locate the .env file)
-from pathlib import Path
-
-# Loading the variables from the .env file by saving the path and giving it to the function load_dotenv()
-env_path = Path(__file__).resolve().parent / "API_key.env"
-load_dotenv(dotenv_path=env_path)
 
 
-api_key = os.getenv("api_key")
-user_input = input("Choose a city: ")
-#  Opening the url of the relevant city using the input and the API key
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
+class Home(QWidget):
 
-if weather_data.json()['cod'] == '404':  # Checking the city was found
-    print("Error: City not found")
-else:
-    weather = weather_data.json()['weather'][0]['main']
-    temp = weather_data.json()['main']['temp']
-    time_zone = weather_data.json()['timezone']  # The offset from UTC (Coordinated universal time) in seconds
-    utc_now = datetime.now(timezone.utc)  # saving the current hour in UTC (Coordinated universal time)
-    local_time = utc_now + timedelta(seconds=time_zone)
-    local_hour = str(local_time.time())[:8]
+    def __init__(self):
+        super().__init__()
+        self.settings()
+        self.initUI()
 
-    print(f"The weather in {user_input} is {weather}, {temp} degrees Celsius")
-    print(f"The local time is {local_hour}")
+    def settings(self):  # A method to define basic visual settings
+        self.setWindowTitle("Weather-Search")
+        self.setGeometry(300, 300, 600, 400)
 
+    def initUI(self):
+        self.title = QLabel("Weather-Search")
+        self.input_box = QLineEdit()
+        self.input_box.setPlaceholderText("Enter your city name")  # Text that will be displayed before searching
 
+        self.output = QLabel("Weather PLACEHOLDER")
+        self.search_button = QPushButton("Search")
+
+        self.desgin = QVBoxLayout()  # This will be the main desgin setting
+        self.desgin.addWidget(self.title)
+        self.desgin.addWidget(self.input_box)
+        self.desgin.addWidget(self.output)
+        self.desgin.addWidget(self.search_button)
+
+        self.setLayout(self.desgin)  # Setting the layout to be based on the main design setting
+
+    def search_click(self):
+        pass
+
+    def search_weather(self, city):
+        weather_data = get_weather(city)
+        if weather_data.status_code == 200:
+            weather_description = weather_data.json()['weather'][0]['main']
+            temp = weather_data.json()['main']['temp']
+            humidity = weather_data.json()['main']['humidity']
+            wind_speed = weather_data.json()['wind']['speed']
+
+            time_zone = weather_data.json()['timezone']  # The offset from UTC (Coordinated universal time) in seconds
+            utc_now = datetime.now(timezone.utc)  # saving the current hour in UTC (Coordinated universal time)
+            local_time = utc_now + timedelta(seconds=time_zone)
+            local_hour = str(local_time.time())[:8]
+
+        elif weather_data.status_code == 404:
+            pass
+
+if __name__ == "__main__":
+    app = QApplication([])
+    main = Home()
+    main.show()
+    main.show
+    app.exec()
---
+++ weather.py
@@ -0,0 +1,29 @@
+import requests  # Imported to have contact with
+from dotenv import load_dotenv  # Importing the function allowing to import data from a .env file
+import os  # Allows Python to interact with the os, needed for the communication with the .env file
+# Importing a command used to navigate to a specific file type (will be used to locate the .env file)
+from pathlib import Path
+
+
+def get_weather(city):
+    # Loading the variables from the .env file by saving the path and giving it to the function load_dotenv()
+    env_path = Path(__file__).resolve().parent / "API_key.env"
+    load_dotenv(dotenv_path=env_path)
+
+
+    api_key = os.getenv("api_key")
+    #  Opening the url of the relevant city using the input and the API key
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={api_key}")
+    # if weather_data.json()['cod'] == 200:
+        # weather = weather_data.json()['weather'][0]['main']
+        # temp = weather_data.json()['main']['temp']
+        # time_zone = weather_data.json()['timezone']  # The offset from UTC (Coordinated universal time) in seconds
+        # utc_now = datetime.now(timezone.utc)  # saving the current hour in UTC (Coordinated universal time)
+        # local_time = utc_now + timedelta(seconds=time_zone)
+        # local_hour = str(local_time.time())[:8]
+        #
+        # print(f"The weather in {city} is {weather}, {temp} degrees Celsius")
+        # print(f"The local time is {local_hour}")
+    return weather_data
+
